cf8c42c52d0e1dbe8afd0d6fece673b7037b0904
Levi-Opunga <leviopunga@gmail.com>
2021-07-16 15:53:29 +0300

    added inputs and submit button to html

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
-function dateInputs() {
-    console.log("Function has been hit!!!");
-    let day = parseInt(document.getElementById("day").value);
-    let month = parseInt(document.getElementById("month").value);
-    let fullYear = document.getElementById("year").value;
-    let century = parseInt(fullYear.slice(0, 2));
-    let year = parseInt(fullYear.slice(-1, -3));
-
-
-    console.log(month);
-    console.log(century);
-    console.log(year);
-
-}
-dateInputs()
-
-function calculateDay(day, month, year, day)
+let
